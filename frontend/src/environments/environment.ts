cf9f574b9d2da317c27a033873c8467633d4c84f
Saliou Cisse <cisse.saliou@courrier.uqam.ca>
2023-12-11 08:28:11 -0500

    Completer Deploiement

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  backendUrl: "http://localhost:8080",
-  wsUrl: "ws://127.0.0.1:8080",
+  backendUrl: "https://chat-app-azpcmaydbq-nn.a.run.app",
+  wsUrl: "wss://chat-app-azpcmaydbq-nn.a.run.app",
 };
